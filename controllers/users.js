68580939ae0b5705f71ad4d1ea6a337d1d4cb3c9
Melih Namer <mnamer12@outlook.com>
2022-10-18 15:39:35 +0300

    adding initial files

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,7 @@
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 
 const castError = (req, res, err) => {
   if (err.name === 'CastError') {
@@ -32,19 +35,49 @@ const getUser = (req, res) => {
     });
 };
 
-const createUser = (req, res) => {
-  const { name, avatar, about } = req.body;
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+  return User.findUserByCredentials(email, password)
+  .then((user) => {
+    const token = jwt.sign(
+      { _id: user._id},
+      NODE_ENV === 'production' ? JWT_SECRET : 'Secret of Narnia',  //in build part just JWT_SECRET
+      { expiresIn: '7d'}
+      );
+    res.send({ data: user.toJSON(), token })    // res.status(200).send(token);
+  })
+  .catch(() => {
+    res.status(401).send({ message: 'Incorrect email or password, please check and try again'});  //new UnauthorizedError //next(new Error('Login information is incorrect, check either email or password'));
+    next();
+  })
+}
 
-  User.create({ name, avatar, about })
-    .then((users) => res.status(201).send({ data: users }))
+
+const createUser = (req, res, next) => {
+  const { name, avatar, about, email, password } = req.body;
+  User.findOne({ email })
+    .then((user) => {
+      if(user) {
+        throw new Error('Current Email has already been registered');  // throw new ErrorRepetitionMail
+      } else {
+        return bcrypt.hash(password, 10);
+      }
+    })
+    .then((hash) =>  User.create({
+      name, avatar, about, email, password: hash,
+    }))
+    .then((users) => res.status(201).send({ data: users })) // send({ users })
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(400).send({ message: 'Bad Request' });
+        res.status(400).send({ message: 'Bad Request' });//next(new BadRequestError(`${Object.values(err.errors).map((error) => error.message).join(', ')}`));
       } else {
-        res.status(500).send({ message: 'Internal Server Error ...' });
+        next(err);//res.status(500).send({ message: 'Internal Server Error ...' });
       }
     });
+
 };
+
+
 
 const updateUserData = (req, res) => {
   const { body } = req;
@@ -84,6 +117,7 @@ const updateUser = (req, res) => {
 module.exports = {
   getAllUsers,
   getUser,
+  login,
   createUser,
   updateAvatar,
   updateUser,
